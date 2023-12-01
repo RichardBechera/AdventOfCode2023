13ae3b043357bc7e1399ebd0362c7604d6ca3a2c
Richard Bechera <richard.bechera@artin.cz>
2023-12-01 16:14:56 +0100

    1.12.2023 trebuchet

---
+++ AdventOfCode/01_trebuchet.fsx
@@ -0,0 +1,20 @@
+open System
+
+let charToInt = int >> (+) (- int '0')  
+
+let getTheNumber (line: string) =
+    let digits = Seq.filter Char.IsDigit line |> Seq.map charToInt
+    match digits with
+    | d when Seq.isEmpty d -> 0
+    | _ -> Seq.head digits * 10 + Seq.last digits
+    
+let trebuchet filePath =
+    if System.IO.File.Exists filePath then 
+        filePath
+        |> System.IO.File.ReadLines
+        |> Seq.fold (fun acc line -> acc + getTheNumber line) 0
+    else
+        -1
+    
+    
+trebuchet "./Input/trebuchet.txt"
